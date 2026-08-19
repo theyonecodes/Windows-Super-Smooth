f697863bfde3c6637f791e6e9b996fc5a6b25dae
theyonecodes <theyonecodes@users.noreply.github.com>
2026-08-20 00:12:38 +0530

    Single file - only WindowsSuperSmooth.ps1

--- Windows-Super-Smooth.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0WindowsSuperSmooth.ps1"
